8db6689e5aa836b493316a2b9242be65511fd18c
katerynaSup <katsup@seas.upenn.edu>
2025-04-10 12:44:59 -0400

    more ingredients, display alc content

--- audio-editor/frontend/src/components/IngredientsSearchPage.jsx
+++ audio-editor/frontend/src/components/IngredientsSearchPage.jsx
@@ -18,6 +18,49 @@ import gingerBeerImage from '../assets/images/ingredients/ginger-beer.png';
 import cranberryJuiceImage from '../assets/images/ingredients/cranberry.png';
 import pineappleJuiceImage from '../assets/images/ingredients/pineapple.png';
 import coconutCreamImage from '../assets/images/ingredients/coconut.png';
+import lemonJuiceImage from '../assets/images/ingredients/lemon-juice.png';
+import agaveSyrupImage from '../assets/images/ingredients/agave-syrup.png';
+import honeySyrupImage from '../assets/images/ingredients/honey.png';
+import basilImage from '../assets/images/ingredients/basil.png';
+import cucumberImage from '../assets/images/ingredients/cucumber.png';
+import blackberryImage from '../assets/images/ingredients/blackberry.png';
+import peachPureeImage from '../assets/images/ingredients/peach.png';
+import eggWhiteImage from '../assets/images/ingredients/egg.png';
+import maraschinoCherryImage from '../assets/images/ingredients/maraschino-cherry.png';
+import oliveImage from '../assets/images/ingredients/olives.png';
+import lycheeSyrupImage from '../assets/images/ingredients/lychee-syrup.png';
+import passionFruitPureeImage from '../assets/images/ingredients/passion-fruit.png';
+import jalapenoSliceImage from '../assets/images/ingredients/jalapeno-slices.png';
+import gingerSyrupImage from '../assets/images/ingredients/ginger-syrup.png';
+import tamarindPasteImage from '../assets/images/ingredients/tamarind.png';
+import chiliPowderImage from '../assets/images/ingredients/chili-powder.png';
+import blueCuracaoImage from '../assets/images/ingredients/blue-curacao.png';
+import roseWaterImage from '../assets/images/ingredients/rose-water.png';
+import lavenderSyrupImage from '../assets/images/ingredients/lavender-syrup.png';
+import matchaPowderImage from '../assets/images/ingredients/matcha-powder.png';
+import greenTeaImage from '../assets/images/ingredients/green-tea.png';
+import sojuImage from '../assets/images/ingredients/soju.png';
+import bacardiImage from '../assets/images/ingredients/bacardi.png';
+import coconutSyrupImage from '../assets/images/ingredients/coconut.png';
+import coconutRumImage from '../assets/images/ingredients/coconut-rum.png';
+import aperolImage from '../assets/images/ingredients/aperol.png';
+import cinnamonSyrupImage from '../assets/images/ingredients/cinnamon.png';
+import sourMixImage from '../assets/images/ingredients/sour-mix.png';
+import orangeBittersImage from '../assets/images/ingredients/orange-bitters.png';
+import sparklingWaterImage from '../assets/images/ingredients/sparkling-water.png';
+import cherryLiqueurImage from '../assets/images/ingredients/cherry-liqueur.jpg';
+import strawberryImage from '../assets/images/ingredients/strawberry.png';
+import strawberryPureeImage from '../assets/images/ingredients/strawberry-puree.png';
+import strawberrySyrupImage from '../assets/images/ingredients/strawberry-syrup.png';
+import strawberryVodkaImage from '../assets/images/ingredients/strawberry-vodka.png';
+import vanillaVodkaImage from '../assets/images/ingredients/vanilla-vodka.png';
+import citrusVodkaImage from '../assets/images/ingredients/citrus-vodka.png';
+import pepperVodkaImage from '../assets/images/ingredients/pepper-vodka.png';
+import blueberryVodkaImage from '../assets/images/ingredients/blueberry-vodka.png';
+import rosemaryImage from '../assets/images/ingredients/rosemary.png';
+import thymeImage from '../assets/images/ingredients/thyme.png';
+import cinnamonStickImage from '../assets/images/ingredients/cinnamon.png';
+import starAniseImage from '../assets/images/ingredients/star-anise.png';
 
 
 // mock data with fixed image paths
@@ -149,6 +192,430 @@ const MOCK_INGREDIENTS = [
         type: 'Dairy Substitute',
         alcohol_content: 0,
         image: coconutCreamImage
+    },
+    {
+        id: 'lemon-juice',
+        name: 'Lemon Juice',
+        description: 'Tart citrus juice that adds brightness and acidity to cocktails.',
+        type: 'Juice',
+        alcohol_content: 0,
+        image: lemonJuiceImage
+    },
+    {
+        id: 'agave-syrup',
+        name: 'Agave Syrup',
+        description: 'A natural sweetener derived from agave plants, often used in tequila cocktails.',
+        type: 'Syrup',
+        alcohol_content: 0,
+        image: agaveSyrupImage
+    },
+    {
+        id: 'honey-syrup',
+        name: 'Honey Syrup',
+        description: 'A mixture of honey and water, used to add smooth sweetness.',
+        type: 'Syrup',
+        alcohol_content: 0,
+        image: honeySyrupImage
+    },
+    {
+        id: 'basil',
+        name: 'Fresh Basil',
+        description: 'A fragrant herb that adds a unique twist to refreshing cocktails.',
+        type: 'Herb',
+        alcohol_content: 0,
+        image: basilImage
+    },
+    {
+        id: 'cucumber',
+        name: 'Cucumber',
+        description: 'Adds a refreshing, cooling element to drinks, often muddled.',
+        type: 'Vegetable',
+        alcohol_content: 0,
+        image: cucumberImage
+    },
+    {
+        id: 'blackberry',
+        name: 'Blackberries',
+        description: 'Juicy and tart berries that add color and flavor.',
+        type: 'Fruit',
+        alcohol_content: 0,
+        image: blackberryImage
+    },
+    {
+        id: 'peach-puree',
+        name: 'Peach Puree',
+        description: 'Smooth and sweet fruit puree used in Bellinis and other fruity drinks.',
+        type: 'Puree',
+        alcohol_content: 0,
+        image: peachPureeImage
+    },
+    {
+        id: 'egg-white',
+        name: 'Egg White',
+        description: 'Used to create a frothy, creamy texture in shaken cocktails.',
+        type: 'Additive',
+        alcohol_content: 0,
+        image: eggWhiteImage
+    },
+    {
+        id: 'maraschino-cherry',
+        name: 'Maraschino Cherry',
+        description: 'A sweet, preserved cherry often used as a garnish.',
+        type: 'Garnish',
+        alcohol_content: 0,
+        image: maraschinoCherryImage
+    },
+    {
+        id: 'olive',
+        name: 'Olive',
+        description: 'Classic savory garnish, especially in martinis.',
+        type: 'Garnish',
+        alcohol_content: 0,
+        image: oliveImage
+    },
+    {
+        id: 'lychee-syrup',
+        name: 'Lychee Syrup',
+        description: 'Sweet, floral syrup made from lychee fruit, great for tropical drinks.',
+        type: 'Syrup',
+        alcohol_content: 0,
+        image: lycheeSyrupImage
+    },
+    {
+        id: 'passion-fruit-puree',
+        name: 'Passion Fruit Puree',
+        description: 'Tart and tangy fruit puree with a tropical punch.',
+        type: 'Puree',
+        alcohol_content: 0,
+        image: passionFruitPureeImage
+    },
+    {
+        id: 'jalapeno-slice',
+        name: 'Jalapeño Slice',
+        description: 'Spicy pepper slice used to add heat and kick to cocktails.',
+        type: 'Spice',
+        alcohol_content: 0,
+        image: jalapenoSliceImage
+    },
+    {
+        id: 'ginger-syrup',
+        name: 'Ginger Syrup',
+        description: 'Sweet and spicy syrup made from fresh ginger root.',
+        type: 'Syrup',
+        alcohol_content: 0,
+        image: gingerSyrupImage
+    },
+    {
+        id: 'tamarind-paste',
+        name: 'Tamarind Paste',
+        description: 'Tart and tangy paste used in exotic and tropical drinks.',
+        type: 'Paste',
+        alcohol_content: 0,
+        image: tamarindPasteImage
+    },
+    {
+        id: 'chili-powder',
+        name: 'Chili Powder',
+        description: 'Spicy and smoky powder used to rim cocktail glasses.',
+        type: 'Garnish',
+        alcohol_content: 0,
+        image: chiliPowderImage
+    },
+    {
+        id: 'blue-curacao',
+        name: 'Blue Curaçao (Non-Alcoholic)',
+        description: 'Citrusy blue liqueur flavoring used for color and flavor in mocktails.',
+        type: 'Flavoring',
+        alcohol_content: 0,
+        image: blueCuracaoImage
+    },
+    {
+        id: 'rose-water',
+        name: 'Rose Water',
+        description: 'Floral aromatic used to add delicate rose essence.',
+        type: 'Flavoring',
+        alcohol_content: 0,
+        image: roseWaterImage
+    },
+    {
+        id: 'lavender-syrup',
+        name: 'Lavender Syrup',
+        description: 'Floral syrup with calming notes, great in herbal or tea-based cocktails.',
+        type: 'Syrup',
+        alcohol_content: 0,
+        image: lavenderSyrupImage
+    },
+    {
+        id: 'matcha-powder',
+        name: 'Matcha Powder',
+        description: 'Finely ground green tea powder used in creative cocktails for earthiness.',
+        type: 'Powder',
+        alcohol_content: 0,
+        image: matchaPowderImage
+    },
+    {
+        id: 'green-tea',
+        name: 'Green Tea',
+        description: 'Light and earthy brewed tea, used in refreshing and antioxidant-rich cocktails.',
+        type: 'Tea',
+        alcohol_content: 0,
+        image: greenTeaImage
+    },
+    {
+        id: 'soju',
+        name: 'Soju',
+        description: 'A clear, low-alcohol Korean spirit traditionally made from rice or sweet potatoes.',
+        type: 'Spirit',
+        alcohol_content: 20,
+        image: sojuImage
+    },
+    {
+        id: 'bacardi',
+        name: 'Bacardi',
+        description: 'A popular brand of white rum with light, crisp notes, great for classic cocktails.',
+        type: 'Spirit',
+        alcohol_content: 40,
+        image: bacardiImage
+    },
+    {
+        id: 'coconut-syrup',
+        name: 'Coconut Syrup',
+        description: 'Sweet syrup made from coconut, adds a creamy, tropical flavor.',
+        type: 'Syrup',
+        alcohol_content: 0,
+        image: coconutSyrupImage
+    },
+    {
+        id: 'coconut-rum',
+        name: 'Coconut Rum',
+        description: 'A rum infused with natural coconut flavor, used in tropical cocktails.',
+        type: 'Spirit',
+        alcohol_content: 21,
+        image: coconutRumImage
+    },
+    {
+        id: 'aperol',
+        name: 'Aperol',
+        description: 'An Italian apéritif with a bittersweet orange flavor and low alcohol content.',
+        type: 'Liqueur',
+        alcohol_content: 11,
+        image: aperolImage
+    },
+    {
+        id: 'cinnamon-syrup',
+        name: 'Cinnamon Syrup',
+        description: 'Sweet and spicy syrup made with cinnamon sticks, adds warmth to drinks.',
+        type: 'Syrup',
+        alcohol_content: 0,
+        image: cinnamonSyrupImage
+    },
+    {
+        id: 'sour-mix',
+        name: 'Sour Mix',
+        description: 'A premade blend of lemon or lime juice and sugar, used in sours and margaritas.',
+        type: 'Mixer',
+        alcohol_content: 0,
+        image: sourMixImage
+    },
+    {
+        id: 'orange-bitters',
+        name: 'Orange Bitters',
+        description: 'Highly concentrated flavoring with citrus and spice notes, used in small dashes.',
+        type: 'Bitters',
+        alcohol_content: 45,
+        image: orangeBittersImage
+    },
+    {
+        id: 'sparkling-water',
+        name: 'Sparkling Water',
+        description: 'Plain carbonated water used to lighten and freshen up cocktails.',
+        type: 'Mixer',
+        alcohol_content: 0,
+        image: sparklingWaterImage
+    },
+    {
+        id: 'cherry-liqueur',
+        name: 'Cherry Liqueur',
+        description: 'A sweet, deep red liqueur made from cherries, often used in tiki drinks and desserts.',
+        type: 'Liqueur',
+        alcohol_content: 30,
+        image: cherryLiqueurImage
+    },
+    {
+        id: 'soju',
+        name: 'Soju',
+        description: 'A clear, low-alcohol Korean spirit traditionally made from rice or sweet potatoes.',
+        type: 'Spirit',
+        alcohol_content: 20,
+        image: sojuImage
+    },
+    {
+        id: 'bacardi',
+        name: 'Bacardi',
+        description: 'A popular brand of white rum with light, crisp notes, great for classic cocktails.',
+        type: 'Spirit',
+        alcohol_content: 40,
+        image: bacardiImage
+    },
+    {
+        id: 'coconut-syrup',
+        name: 'Coconut Syrup',
+        description: 'Sweet syrup made from coconut, adds a creamy, tropical flavor.',
+        type: 'Syrup',
+        alcohol_content: 0,
+        image: coconutSyrupImage
+    },
+    {
+        id: 'coconut-rum',
+        name: 'Coconut Rum',
+        description: 'A rum infused with natural coconut flavor, used in tropical cocktails.',
+        type: 'Spirit',
+        alcohol_content: 21,
+        image: coconutRumImage
+    },
+    {
+        id: 'aperol',
+        name: 'Aperol',
+        description: 'An Italian apéritif with a bittersweet orange flavor and low alcohol content.',
+        type: 'Liqueur',
+        alcohol_content: 11,
+        image: aperolImage
+    },
+    {
+        id: 'cinnamon-syrup',
+        name: 'Cinnamon Syrup',
+        description: 'Sweet and cuspicy syrup made with cinnamon sticks, adds warmth to drinks.',
+        type: 'Syrup',
+        alcohol_content: 0,
+        image: cinnamonSyrupImage
+    },
+    {
+        id: 'sour-mix',
+        name: 'Sour Mix',
+        description: 'A premade blend of lemon or lime juice and sugar, used in sours and margaritas.',
+        type: 'Mixer',
+        alcohol_content: 0,
+        image: sourMixImage
+    },
+    {
+        id: 'orange-bitters',
+        name: 'Orange Bitters',
+        description: 'Highly concentrated flavoring with citrus and spice notes, used in small dashes.',
+        type: 'Bitters',
+        alcohol_content: 45,
+        image: orangeBittersImage
+    },
+    {
+        id: 'sparkling-water',
+        name: 'Sparkling Water',
+        description: 'Plain carbonated water used to lighten and freshen up cocktails.',
+        type: 'Mixer',
+        alcohol_content: 0,
+        image: sparklingWaterImage
+    },
+    {
+        id: 'cherry-liqueur',
+        name: 'Cherry Liqueur',
+        description: 'A sweet, deep red liqueur made from cherries, often used in tiki drinks and desserts.',
+        type: 'Liqueur',
+        alcohol_content: 30,
+        image: cherryLiqueurImage
+    },
+    {
+        id: 'strawberry',
+        name: 'Strawberry',
+        description: 'Fresh, juicy fruit often muddled or used as a garnish for sweet and fruity cocktails.',
+        type: 'Fruit',
+        alcohol_content: 0,
+        image: strawberryImage
+    },
+    {
+        id: 'strawberry-puree',
+        name: 'Strawberry Puree',
+        description: 'Smooth and sweet puree made from ripe strawberries, perfect for daiquiris and margaritas.',
+        type: 'Puree',
+        alcohol_content: 0,
+        image: strawberryPureeImage
+    },
+    {
+        id: 'strawberry-syrup',
+        name: 'Strawberry Syrup',
+        description: 'Sweet and vibrant syrup used to flavor lemonades, sodas, and cocktails.',
+        type: 'Syrup',
+        alcohol_content: 0,
+        image: strawberrySyrupImage
+    },
+    {
+        id: 'strawberry-vodka',
+        name: 'Strawberry Vodka',
+        description: 'Vodka infused with strawberry flavor, perfect for fruity and refreshing drinks.',
+        type: 'Spirit',
+        alcohol_content: 35,
+        image: strawberryVodkaImage
+    },
+    {
+        id: 'vanilla-vodka',
+        name: 'Vanilla Vodka',
+        description: 'Flavored vodka with a smooth vanilla finish, used in dessert-style cocktails.',
+        type: 'Spirit',
+        alcohol_content: 35,
+        image: vanillaVodkaImage
+    },
+    {
+        id: 'citrus-vodka',
+        name: 'Citrus Vodka',
+        description: 'Vodka infused with lemon, lime, or orange peels for a bright and zesty twist.',
+        type: 'Spirit',
+        alcohol_content: 35,
+        image: citrusVodkaImage
+    },
+    {
+        id: 'pepper-vodka',
+        name: 'Pepper Vodka',
+        description: 'Spicy vodka infused with black pepper or chili, often used in savory drinks like Bloody Marys.',
+        type: 'Spirit',
+        alcohol_content: 35,
+        image: pepperVodkaImage
+    },
+    {
+        id: 'blueberry-vodka',
+        name: 'Blueberry Vodka',
+        description: 'Fruity vodka infused with blueberry flavor, great in summer drinks.',
+        type: 'Spirit',
+        alcohol_content: 35,
+        image: blueberryVodkaImage
+    },
+    {
+        id: 'rosemary',
+        name: 'Fresh Rosemary',
+        description: 'Piney herb that adds an herbal depth and aroma to cocktails.',
+        type: 'Herb',
+        alcohol_content: 0,
+        image: rosemaryImage
+    },
+    {
+        id: 'thyme',
+        name: 'Fresh Thyme',
+        description: 'Fragrant herb that pairs well with gin, vodka, and citrus elements.',
+        type: 'Herb',
+        alcohol_content: 0,
+        image: thymeImage
+    },
+    {
+        id: 'cinnamon-stick',
+        name: 'Cinnamon Stick',
+        description: 'Used for garnish or muddled to add warm spice to fall or winter cocktails.',
+        type: 'Spice',
+        alcohol_content: 0,
+        image: cinnamonStickImage
+    },
+    {
+        id: 'star-anise',
+        name: 'Star Anise',
+        description: 'Aromatic spice shaped like a star, adds a subtle licorice note.',
+        type: 'Spice',
+        alcohol_content: 0,
+        image: starAniseImage
     }
 ];
 
@@ -274,7 +741,7 @@ const IngredientsSearchPage = ({
                 {filteredIngredients.length > 0 ? (
                     filteredIngredients.map(ingredient => (
                         <div key={ingredient.id} className="card">
-                            < img
+                            <img
                                 src={ingredient.image}
                                 alt={ingredient.name}
                                 className="card-image"
@@ -285,7 +752,14 @@ const IngredientsSearchPage = ({
                             />
                             <div className="card-content">
                                 <h2 className="body-text">{ingredient.name}</h2>
-                                <p className="ingredient-type">{ingredient.type}</p>
+                                <div className="ingredient-details">
+                                    <p className="ingredient-type">{ingredient.type}</p>
+                                    <p className="ingredient-alcohol">
+                                        {ingredient.alcohol_content > 0
+                                            ? `${ingredient.alcohol_content}% ABV`
+                                            : 'Non-alcoholic'}
+                                    </p>
+                                </div>
                                 <button
                                     onClick={() => handleAddToBar(ingredient)}
                                     className="button primary-button"
